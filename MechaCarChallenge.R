0ede3f358da6a8b19e8d3c7cc14f7ae2c940af1c
Deelacole2 <75325008+Deelacole2@users.noreply.github.com>
2021-03-28 13:27:15 -0500

    Adding files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,16 @@ head(suspension_coil)
 total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD =  sd(PSI), .groups = 'keep')
 # Statistical summary of the suspension coil data frame grouped my lot number.
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD =  sd(PSI), .groups = 'keep')
+
+#MechaCar Suspension Coil t-test
+overall_ttest <- t.test(log10(suspension_coil$PSI), mu=mean(log10(suspension_coil$PSI)))
+
+# Manufacturer Lot subsetted tables
+Lot1_summary <- subset(suspension_coil,Manufacturing_Lot=='Lot1') %>% group_by(Manufacturing_Lot) %>% summarize(Lot1_PSI_mean=mean(PSI), .groups = 'keep')
+Lot2_summary <- subset(suspension_coil,Manufacturing_Lot=='Lot2') %>% group_by(Manufacturing_Lot) %>% summarize(Lot2_PSI_mean=mean(PSI), .groups = 'keep')
+Lot3_summary <- subset(suspension_coil,Manufacturing_Lot=='Lot3') %>% group_by(Manufacturing_Lot) %>% summarize(Lot3_PSI_mean=mean(PSI), .groups = 'keep')
+
+#T.test for each lot compared to the population mean
+t.test(log10(suspension_coil$PSI), mu=mean(log10(Lot1_summary$Lot1_PSI_mean)))
+t.test(log10(suspension_coil$PSI), mu=mean(log10(Lot2_summary$Lot2_PSI_mean)))
+t.test(log10(suspension_coil$PSI), mu=mean(log10(Lot3_summary$Lot3_PSI_mean)))
